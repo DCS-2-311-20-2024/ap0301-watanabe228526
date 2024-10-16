c3015ecb8fd260244bde404ae254a9ce48c92d30
G285262022 <228526@st.takushoku-u.ac.jp>
2024-10-16 17:12:02 +0900

    第3回前半
    ロボットの回転
    ロボットの追加

--- ap0301.js
+++ ap0301.js
@@ -1,6 +1,6 @@
 //
 // 応用プログラミング 第3回 課題1 (ap0301)
-// G384002023 拓殖太郎
+// G285262022 渡邉秋
 //
 "use strict"; // 厳格モード
 
@@ -25,11 +25,24 @@ function init() {
   scene.add(axes);
 
   // ロボットの作成
-  const robot = makeMetalRobot();
-  scene.add(robot);
-
+  const robots=new THREE.Group;
+  for(let x=-4;x<=4;x++){
+    for(let z=-4;z<=4;z++){
+      let robot;
+      if(Math.random()<0.25){
+        robot=makeCBRobot();
+      }else{
+        robot=makeMetalRobot()
+      }
+      robot.position.x=x*6;
+      robot.position.z=z*6;
+      robot.rotation.y=Math.atan2(x,z);
+      robots.add(robot);
+    }
+  }
+  scene.add(robots);
   // 光源の設定
-  const light = new THREE.SpotLight();
+  const light = new THREE.SpotLight(0xffffff,1800);
   light.position.set(0, 30, 30);
   scene.add(light);
   
@@ -52,15 +65,22 @@ function init() {
     camera.position.z = param.z;
     camera.lookAt(0, 0, 0);
     camera.updateProjectionMatrix();
-    renderer.render(scene, camera);
+  //ロボットの動き
+  robots.children.forEach((robot)=>{
+    robot.rotation.y
+      =(robot.rotation.y+0.01)%(2*Math.PI);
+    robot.position.y=Math.sin(robot.rotation.y);
+  });
+    renderer.render(scene,camera);
+    requestAnimationFrame(render);
   }
 
   // カメラのコントローラ
   const gui = new GUI();
-  gui.add(param, "fov", 10, 100).onChange(render);
-  gui.add(param, "x", -50, 50).onChange(render);
-  gui.add(param, "y", -50, 50).onChange(render);
-  gui.add(param, "z", -50, 50).onChange(render);
+  gui.add(param, "fov", 10, 100);
+  gui.add(param, "x", -50, 50);
+  gui.add(param, "y", -50, 50);
+  gui.add(param, "z", -50, 50);
   
   // 描画
   render();
